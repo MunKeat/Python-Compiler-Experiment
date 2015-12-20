564b569aceb24f046a825f37bf6c7fe020da25f5
MunKeat <a0129561@u.nus.edu>
2015-12-20 14:54:43 +0800

    Modified lex.py
    1. Replace list of dictionary in token_dict to simply dictionary
    2. Added debug flag for define(), and analyse()
    3. Allow tokenise() to skip whitespace

--- Lexical Analyzer/lex.py
+++ Lexical Analyzer/lex.py
@@ -13,26 +13,23 @@ Acknowledgement:
 # Import regular expression module to parse definition file
 import re
 
-# Permits usage of a dictionary of lists
-from collections import defaultdict
-
 # Keep track of line number in case of error diagnosis
 line_number = 1
 # File which contains source code
 # TODO: Ensures that script accept parameter
-source = open("file.txt", "r")
+source = open("file.txt", "br+")
 # File which contains definition for tokens
 # TODO: Set up a minimal version of Definition.txt, for HereDoc, to allow lex.py to be independent
 definition_text = "SampleDefinition.txt"
-# Dictionary of list containing tokens
-token_dict = defaultdict(list)
+# Dictionary containing tokens
+token_dict = {}
 
 ########################################
 # Definition
 ########################################
-def define(self, file):
+def define(debug=False):
     file = open(definition_text)
-    for line in self.file:
+    for line in file:
         # Strip trailing newline characters
         line = line.rstrip("\n")
         # Split using regular expression at '::='
@@ -40,18 +37,41 @@ def define(self, file):
         tokenItem = tokenDef[0]
         # Split using regular expression at '|'
         tokenRules = re.split("\s+\|\s+", tokenDef[1])
-            # Generate dictionary of rules for language
-            for rule in tokenRules:
-                # The following assumption will hold:
-                # separator, whitepace & newline are 1 character
-                token_dict[rule].append(tokenItem)
+        # Generate dictionary of rules for language
+        for rule in tokenRules:
+            # The following assumption will hold:
+            # separator, whitepace & newline are 1 character
+            token_dict[str(rule)] = tokenItem
+    if debug:
+        for key, value in token_dict.items():
+            print("{} : {}".format(key, value))
 
-# For debugging
-def _analyse(self, source):
-# Analyse
+def is_whitespace(character):
+    if character in token_dict:
+        return token_dict[character] == "spaceToken"
+    else:
+        return False
 
-def output(self):
-# _analyse(source)
+def is_separator(character):
+    if character in token_dict:
+        return token_dict[character] == "separatorToken"
+    else:
+        return False
+
+def is_newline(character):
+    if character in token_dict:
+        return token_dict[character] == "newlineToken"
+    else:
+        return False
+
+def is_num(character):
+    return str(character).isdigit()
+
+def is_alpha(character):
+    return str(character).isalpha()
+
+def is_alnum(character):
+    return str(character).isalpha() or str(character).isdigit()
 
 ########################################
 # Tokenise
@@ -62,7 +82,7 @@ def read_next():
     (Also increment file's current position by one)
     '''
     character = source.read(1)
-    return character
+    return character.decode("ascii")
 
 def peek():
     '''
@@ -70,12 +90,66 @@ def peek():
     (Note that this does not affect file's current position)
     '''
     character = read_next()
-    file.seek(-1, 1)
+    source.seek(-1, 1)
     return character
 
-def tokenise():
+def tokenise(skip_whitespace=True):
     '''Tokenise a stream of tokens (or more accurately, terminals)'''
     # Check if dictionary is available
-    if not tokens_dict:
-        # instatantiate class lexer, then analyse
+    if not token_dict:
+        define()
+    char = read_next()
+    token = []
+    # Case: Alphabet
+    if is_alpha(char):
+        token.append(char)
+        while True:
+            temp = peek()
+            if not is_alnum(temp):
+                token = "".join(token)
+                return (token, "string")
+            else:
+                token.append(read_next())
+    # Case : Numbers
+    elif is_num(char):
+        token.append(char)
+        while True:
+            temp = peek()
+            if not is_num(temp):
+                token = "".join(token)
+                return (token, "number")
+            else:
+                token.append(read_next())
+    # Case: Whitespace
+    elif is_whitespace(char):
+        if skip_whitespace:
+            return
+        else:
+            return (char, token_dict[char])
+    # Case: Separator
+    elif is_separator(char):
+        return (char, token_dict[char])
+    # Case: Newline
+    elif is_newline(char):
+        line_number += 1
+        return (char, token_dict[char])
+    # Case: Default
+    else:
+        if char in token_dict:
+            return (char, token_dict[char])
+        else:
+            return (char, "UNKNOWN")
 
+# For debugging
+def analyse(output=False):
+    # List of tokens and corresponding type
+    source_tokenised = []
+    while peek():
+        source_tokenised.append(tokenise())
+    if output:
+        for source_token in source_tokenised:
+            print(source_token)
+    return source_tokenised
+
+define()
+analyse()
